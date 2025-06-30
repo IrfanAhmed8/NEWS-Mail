5c764f854d2b0993ec9f5057c672941eed67c41f
IrfanAhmed8 <jafriirfan37@gmail.com>
2025-06-30 19:19:32 +0530

    authentication added

--- BBCdb.py
+++ BBCdb.py
@@ -1,4 +1,5 @@
 from config import db
+import bcrypt
 
 class BBC(db.Model):
     title = db.Column(db.String(100),primary_key=True)
@@ -14,3 +15,20 @@ class Business(db.Model):
     content = db.Column(db.Text)
     image=db.Column(db.String(100))
     summary = db.Column(db.Text)
+
+
+
+class User(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(300),nullable=True)
+    email=db.Column(db.String(300),unique=True)
+    password=db.Column(db.String(500),nullable=True)
+      
+    def __init__(self,name,email,password):
+        self.name=name
+        self.email=email
+        self.password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+
+    def check_password(self,password):
+        return bcrypt.checkpw(password.encode('utf-8'), self.password.encode('utf-8'))
+        
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # NEWS-Mail
 An app which list current news.user can subscribed to daily/ weekly news mail.all important daily/news will be emailed to user.
+working
